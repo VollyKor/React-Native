7388f0da97b7b2c4fe9592745e376315e1a10baf
VolKor <vol.kor.93@gmail.com>
2021-02-16 16:53:00 +0300

    1

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 
 export default function App() {
   console.log("Hello World");
   return (
+    // <ImageBackground source={require("./assets/PhotoBG.jpg")}>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    // </ImageBackground>
   );
 }
 
